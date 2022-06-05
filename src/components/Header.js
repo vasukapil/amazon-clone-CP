d0ef1014861ba82564cf9afc18d17a6999146420
vasukapil <vasukapil2196@gmail.com>
2022-06-05 19:23:42 +0530

    2nd hour

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './Header.css';
 import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function Header() {
   return (
@@ -33,6 +34,14 @@ function Header() {
           <span className="Header__optionsLineOne">Your</span>
           <span className="Header__optionsLineTwo">Prime</span>
         </div>
+        <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__optionLineTwo header__basketCount">
+              0
+            </span>
+
+
+            </div>
       </div>
     </div>
   );
